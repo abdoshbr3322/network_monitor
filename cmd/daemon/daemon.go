8ec91a15a208595b5af728eeaf10164c35583f9d
Abdullah <abdullahelshebrawy@gmail.com>
2025-11-21 11:21:37 +0200

    daemon

--- cmd/daemon/daemon.go
+++ cmd/daemon/daemon.go
@@ -1,15 +1,56 @@
 package main
 
-import "github.com/abdoshbr3322/network_monitor/internal/utils"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/abdoshbr3322/network_monitor/internal/collect"
+	"github.com/abdoshbr3322/network_monitor/internal/database"
+	"github.com/abdoshbr3322/network_monitor/internal/types"
+	"github.com/abdoshbr3322/network_monitor/internal/utils"
+)
+
+func Crash(err error) {
+	fmt.Fprintf(os.Stderr, "Error, %s\n", err)
+	os.Exit(-1)
+}
 
 func main() {
-	// rx, tx, err := collect.CollectNetworkStats()
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error in collecting data : %s", err)
-	// 	os.Exit(-1)
-	// }
-	// fmt.Println(rx, tx)
-	// collect
-	// collec
-	utils.GetCurrentDay()
+	db, err := database.OpenSQLite()
+	if err != nil {
+		Crash(err)
+	}
+	defer db.Close()
+	err = database.InitSQLite(db)
+	if err != nil {
+		Crash(err)
+	}
+	err = database.PrepareDailyMonthlyStats(db)
+	if err != nil {
+		Crash(err)
+	}
+
+	initial_daily_stats, _ := database.GetDailyStats(db, utils.GetCurrentDay())
+	initial_monthly_stats, _ := database.GetMonthlyStats(db, utils.GetCurrentMonth())
+	initial_stats, err := collect.CollectNetworkStats()
+
+	if err != nil {
+		Crash(err)
+	}
+
+	for {
+		current_stats, err := collect.CollectNetworkStats()
+		if err != nil {
+			Crash(err)
+		}
+		current_stats.RX_bytes -= initial_stats.RX_bytes
+		current_stats.TX_bytes -= initial_stats.TX_bytes
+		err = database.UpdateStats(db, types.Stats.Add(current_stats, initial_monthly_stats), types.Stats.Add(initial_daily_stats, current_stats))
+		if err != nil {
+			Crash(err)
+		}
+		time.Sleep(2 * time.Second)
+	}
+
 }
--- internal/types/types.go
+++ internal/types/types.go
@@ -4,3 +4,7 @@ type Stats struct {
 	RX_bytes int64
 	TX_bytes int64
 }
+
+func (st Stats) Add(st_2 Stats) Stats {
+	return Stats{st.RX_bytes + st_2.RX_bytes, st.TX_bytes + st_2.RX_bytes}
+}
